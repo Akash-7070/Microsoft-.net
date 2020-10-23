65fede1a6a0561793b12af97cb13e84671b3a91a
Akash Kavade <63800325+Akash-7070@users.noreply.github.com>
2020-10-23 21:19:11 +0530

    Add files via upload

---
+++ Day-5/Que-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClassLAmt;
+namespace AmountClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Vaibhav", 50000);
+            a1.deposite(10000);
+            a1.withdraw(10000);
+            a1.Display();
+
+            Account a2 = new Account("Ganesh", 50000);
+            a2.deposite(10000);
+            a2.withdraw(10000);
+            a2.disp();
+
+            Account a3 = new Account("Akash", 50000);
+            a3.deposite(10000);
+            a3.withdraw(10000);
+            a3.Display();
+
+            Account a4 = new Account("Anand", 50000);
+         
+        }
+    }
+}
