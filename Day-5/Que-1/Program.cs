418a3e9af5b1a26cb4d12acc7fb793c74d596569
Akash Kavade <63800325+Akash-7070@users.noreply.github.com>
2020-10-23 21:20:51 +0530

    Add files via upload

---
+++ Day-5/Que-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Classl;
+namespace cl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("Akash", "Kavade");
+            Employee e2 = new Employee("Captain", "Jack", "Sparrow");
+            e1.Display();
+            e2.Display();
+            Console.ReadLine();
+        }
+    }
+}
