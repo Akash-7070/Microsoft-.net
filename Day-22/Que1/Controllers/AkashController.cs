350a91adbcc701792f59a8e518640d57941a3415
Akash Kavade <63800325+Akash-7070@users.noreply.github.com>
2020-11-13 20:23:56 +0530

    Add files via upload

---
+++ Day-22/Que1/Controllers/AkashController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_1.Controllers
+{
+    public class AkashController : Controller
+    {
+        //
+        // GET: /Akash/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
